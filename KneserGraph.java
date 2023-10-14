15da000ef611dce44102aa183f24ce39cd632723
Mikhail Azaryan <mikhailazaryan1514@gmail.com>
2023-10-14 23:13:20 +0200

    Initial commit

---
+++ KneserGraph.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class KneserGraph {
+
+    //protected ArrayList set;
+    protected int n;
+    protected int k;
+
+    public KneserGraph (int n, int k) {
+        //this.set = set;
+        this.n = n;
+        this.k = k;
+        this.initialize();
+    }
+
+    private void initialize () {
+        //TODO for arbitrary integers and other data types
+    }
+
+
+
+}
